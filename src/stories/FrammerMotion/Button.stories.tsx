4db295560f9271b413930a2db6a27eba00eddf32
fcxmarquez <fcxmarquez@MacBook-Pro-de-Francisco.local>
2022-03-10 16:40:30 -0600

    FrammerMotion First Commit

---
+++ src/stories/FrammerMotion/Button.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { Button } from '../../buttons/Button';
+import { ButtonProps } from '../../buttons/Button';
+
+export default {
+  title: 'FrammerMotion/Button',
+  component: Button,
+} as Meta;
+
+const ref = React.createRef<HTMLButtonElement>();
+
+const Template: Story<ButtonProps> = (args) => (
+  <Button ref={ref} {...args}>
+    My button component
+  </Button>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  variant: 'primary',
+  size: 'medium',
+  isFullWidth: false,
+};
